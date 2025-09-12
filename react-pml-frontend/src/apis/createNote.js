4fc869fee0eef15495dac1f8ac7312833b3c0b1a
OHXTRG <119294244+OHXTRG@users.noreply.github.com>
2025-09-12 09:01:44 +0530

    changes in create note api frontend

--- react-pml-frontend/src/apis/createNote.js
+++ react-pml-frontend/src/apis/createNote.js
@@ -3,6 +3,7 @@ import axios from "axios";
 export const createNote = async (values, noteModule) => {
   const response = await axios.post(
     `${process.env.REACT_APP_API_URL}notes/${noteModule}/addNote`,
+    // `${process.env.REACT_APP_API_URL}notes/dailybriefings/addNote`,
     values
   );
   return response.data;
